43cd2ef92f460ea77ec8a7096fb5c1d02a84a281
Shawn Thompson <plansmash@me.com>
2022-04-05 18:43:26 -0400

    create CNAME file and pass it to site folder

--- .eleventy.js
+++ .eleventy.js
@@ -9,7 +9,7 @@ const markdownItAnchor = require("markdown-it-anchor");
 module.exports = function(eleventyConfig) {
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   // Add plugins
   eleventyConfig.addPlugin(pluginRss);
