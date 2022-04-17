160cd004548ad90419e26c38df1bb73cd4068801
Shawn Thompson <plansmash@me.com>
2022-04-17 13:49:01 -0400

    organized source files

--- .eleventy.js
+++ .eleventy.js
@@ -128,7 +128,7 @@ module.exports = function(eleventyConfig) {
 
     // These are all optional (defaults are shown):
     dir: {
-      input: ".",
+      input: "./_src",
       includes: "_includes",
       data: "_data",
       output: "_site"
